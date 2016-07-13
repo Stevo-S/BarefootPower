73a161c9786f291ce95547bca884cf1756b33d8a
Steve Savio <steve@bettersms.net>
2016-07-13 14:10:56 +0300

    Add ability to Filter SaleRegistrations

---
+++ BarefootPower/Models/SalesRegistrationExport.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BarefootPower.Models
+{
+    public class SalesRegistrationExport
+    {
+        public DateTime Date { get; set; }
+        public string Location { get; set; }
+        public string REA { get; set; }
+        public string GroupName { get; set; }
+        public int TotalMembership { get; set; }
+        public int Present { get; set; }
+        public int WithElec { get; set; }
+        public int WithSolar { get; set; }
+        public virtual Agent Agent { get; set; }
+        public virtual ICollection<Sale> Sales { get; set; }
+    }
+}
